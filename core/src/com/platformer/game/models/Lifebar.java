abe4bf5349def949a74dbbd239d9bbddc833bdb0
unknown <chams.tifoura@hotmail.fr>
2023-04-25 14:40:45 +0200

    ok

---
+++ core/src/com/platformer/game/models/Lifebar.java
@@ -0,0 +1,12 @@
+package com.platformer.game.models;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Lifebar {
+    
+}
